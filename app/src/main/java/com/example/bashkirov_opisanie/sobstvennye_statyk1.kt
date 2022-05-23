cdff3b9c68cec99ac387dbd8d003d5660be2b61f
Vyacheslav <94893819+6xisp1@users.noreply.github.com>
2022-05-23 14:20:58 +0700

    proekt_23.05

--- app/src/main/java/com/example/bashkirov_opisanie/sobstvennye_statyk1.kt
+++ app/src/main/java/com/example/bashkirov_opisanie/sobstvennye_statyk1.kt
@@ -3,9 +3,9 @@ package com.example.bashkirov_opisanie
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class sobstvennye_statyk : AppCompatActivity() {
+class sobstvennye_statyk1 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sobstvennye_static)
+        setContentView(R.layout.activity_sobstvennye_pppoe)
     }
 }
