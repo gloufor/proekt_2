b14b12d89a6fa23ee31aef4252beaae7024fff7e
Vyacheslav <94893819+6xisp1@users.noreply.github.com>
2022-05-27 14:19:09 +0700

    proekt_27.05

--- app/src/main/java/com/example/bashkirov_opisanie/opisanie.kt
+++ app/src/main/java/com/example/bashkirov_opisanie/opisanie.kt
@@ -3,7 +3,6 @@ package com.example.bashkirov_opisanie
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import android.widget.ImageView
 
 class opisanie : AppCompatActivity() {
